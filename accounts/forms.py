0a560d288da7c3577c0f62f16a77b9e14b887140
양다은 <yang_daeun@yangda-eun-ui-MacBookPro.local>
2025-04-09 13:43:54 +0900

    이름 필드에 숫자와 특수문자 제한 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,7 @@ class SignUpForm(UserCreationForm):
         max_length=30, 
         required=True, 
         widget=forms.TextInput(attrs={'class': 'form-control'}),
-        help_text='실명을 입력해주세요.'
+        help_text='실명을 입력해주세요. 한글과 영문자만 사용 가능합니다.'
     )
     
     class Meta:
@@ -31,6 +31,13 @@ class SignUpForm(UserCreationForm):
         if not re.match(r'^[a-zA-Z0-9@.+\-_]+$', username):
             raise ValidationError('사용자명은 영문자, 숫자, 특수문자(@/./+/-/_)만 사용 가능합니다. 한글이나 다른 특수문자는 사용할 수 없습니다.')
         return username
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        # 한글, 영문자만 허용하는 정규식
+        if not re.match(r'^[가-힣a-zA-Z\s]+$', first_name):
+            raise ValidationError('이름은 한글과 영문자만 입력 가능합니다. 숫자나 특수문자는 사용할 수 없습니다.')
+        return first_name
         
     def save(self, commit=True):
         user = super().save(commit=False)
