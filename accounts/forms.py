4cfbe43e96303066443dd7b41d9d79c05f8ec761
양다은 <yang_daeun@yangda-eun-ui-MacBookPro.local>
2025-04-09 13:35:58 +0900

    아이디 입력 시 한글 불가능하도록 유효성 검사 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+import re
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
@@ -15,6 +17,15 @@ class SignUpForm(UserCreationForm):
         self.fields['username'].widget.attrs.update({'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'class': 'form-control'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        # 사용자명 필드에 도움말 추가
+        self.fields['username'].help_text = '영문자, 숫자, 특수문자(@/./+/-/_)만 사용 가능합니다. 한글은 사용할 수 없습니다.'
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        # 영문자, 숫자, 특수문자(@/./+/-/_)만 허용하는 정규식
+        if not re.match(r'^[a-zA-Z0-9@.+\-_]+$', username):
+            raise ValidationError('사용자명은 영문자, 숫자, 특수문자(@/./+/-/_)만 사용 가능합니다. 한글이나 다른 특수문자는 사용할 수 없습니다.')
+        return username
         
     def save(self, commit=True):
         user = super().save(commit=False)
